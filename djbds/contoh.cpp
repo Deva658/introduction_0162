a3830ea331cfb115e1729f1f1aadde9a4a70d30c
Deva658 <devaaditya271@gmail.com>
2025-06-08 23:15:59 +0700

    menambahkan lagu

--- djbds/contoh.cpp
+++ djbds/contoh.cpp
@@ -5,3 +5,20 @@
 #include <string>
 
 using namespace std;
+
+void print_lirik(){
+    vector<pair<string,double>> 
+	baris= {
+        {"how can i move on", 0.1},
+        {"When i'm still in love with YOU?", 0.04},
+        {"'Cause if one day you wake up", 0.06},
+        {"And find that you're missing me", 0.04},
+        {"And your heart start to wonder", 0.06},
+        {"Where on this earth i could be", 0.05},
+        {"Thinking maybe you'll come back", 0.05},
+        {"Here to the place that we'd meet", 0.04},
+        {"And you'll see me waiting for you", 0.05},
+        {"On the corner of the street", 0.05},
+        {"SO IM NOT MOVING", 0.13},
+        {"IM NOT MOOVING", 0.22}
+    };
